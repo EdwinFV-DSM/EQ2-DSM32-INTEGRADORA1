ce6e25896369de03402095cdb88840d9152c4c01
Edwin Flores Vargas <al222110833@gmail.com>
2022-06-28 07:33:01 -0500

    Creacion de la carpeta config para conexion a base de datos

---
+++ config/ConexionDB.php
@@ -0,0 +1,14 @@
+<?php 
+ try {
+    $PDO = new PDO('mysql:host=localhost; dbname=treninterurbano','root','');
+    } catch (PDOException $e) {
+        echo "Error!!".$e->getMessage();
+   }
+   
+   $conexion = new mysqli("localhost", "root", "", "treninterurbano");
+
+   if ($conexion) {
+    echo "La conexion fue exitosa a la base de datos";
+   }else{
+    echo "No se pudo conectar a la base de datos";
+   }
