dfb4ddbca8a79e0e4141081414628dfec176bea6
Edwin Flores Vargas <al222110833@gmail.com>
2022-07-10 12:12:49 -0500

    Creacion del fichero index.php que sera la primera vista del usuario

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+include_once 'includes/header.php';
+?>
+
+
+
+<!-- Inicio del Contenido de usuario -->
+<div class="about-us">
+    <div class="container">
+        <div class="row shadow-lg p-3 mb-5 bg-body rounded ">
+            <h3>About Us</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque sint provident odio nobis modi nostrum
+                nesciunt commodi est. Minus explicabo quidem porro magni eos magnam minima nihil, non laborum
+                perspiciatis?</p>
+            <div class="iconos-about">
+                <div class="clientes" style="text-align: center;">
+                    <img src="assets/img/descargar.png" alt="">
+                    <h6>Clientes</h6>
+                    <p>unknown printer took a galley of type</p>
+                </div>
+                <div class="presencia" style="text-align: center;">
+                    <img src="assets/img/descargar (1).png" alt="">
+                    <h6>Presencia</h6>
+                    <p>unknown printer took a galley of type</p>
+                </div>
+                <div class="colaboradores" style="text-align: center;">
+                    <img src="assets/img/descargar (2).png" alt="">
+                    <h6>Colaboradores</h6>
+                    <p>unknown printer took a galley of type</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="innovative">
+    <h3>INNOVATIVE TRANSPORT S.A DE C.V.</h3>
+
+</div>
+<!-- Fin del Contenido de usuario -->
+
+
+
+<?php include_once 'includes/footer.php';
