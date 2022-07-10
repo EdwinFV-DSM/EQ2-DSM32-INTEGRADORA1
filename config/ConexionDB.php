2bbe619070a946263c31d713e22e4c3213fa50b2
Edwin Flores Vargas <al222110833@gmail.com>
2022-07-10 12:13:16 -0500

    Modificacion de la conexion de la base de datos, donde se conecto al servidor remoto

--- config/ConexionDB.php
+++ config/ConexionDB.php
@@ -1,11 +1,27 @@
 <?php 
- try {
-    $PDO = new PDO('mysql:host=localhost; dbname=treninterurbano','root','');
+//  try {
+//     $PDO = new PDO('mysql:host=localhost; dbname=treninterurbano','root','');
+//     } catch (PDOException $e) {
+//         echo "Error!!".$e->getMessage();
+//    }
+   
+//    $conexion = new mysqli("localhost", "root", "", "treninterurbano");
+
+//    if ($conexion) {
+//     echo "La conexion fue exitosa a la base de datos";
+//    }else{
+//     echo "No se pudo conectar a la base de datos";
+//    }
+
+//    mysqli_close($conexion);
+
+try {
+    $PDO = new PDO('mysql:host=www.db4free.net; dbname=treninterurbano; port=3306','admintren','admin2022');
     } catch (PDOException $e) {
         echo "Error!!".$e->getMessage();
    }
    
-   $conexion = new mysqli("localhost", "root", "", "treninterurbano");
+   $conexion = new mysqli("www.db4free.net", "admintren", "admin2022", "treninterurbano",3306);
 
    if ($conexion) {
     echo "La conexion fue exitosa a la base de datos";
