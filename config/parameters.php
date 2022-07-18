16ae987cbf9b7850a23828fe0c732c65d6ecc7b2
Edwin Flores Vargas <al222110833@gmail.com>
2022-07-17 20:39:23 -0500

    Se agregaron mas base_url para que todos los paneles de administracion tengan acceso a los estilos

--- config/parameters.php
+++ config/parameters.php
@@ -1,4 +1,6 @@
 <?php
 define("base_url", "http://localhost/EQ2-DSM32-INTEGRADORA1/");
+define("base_url_admin", "http://localhost/EQ2-DSM32-INTEGRADORA1/admin");
+define("base_url_trabajador", "http://localhost/EQ2-DSM32-INTEGRADORA1/trabajador");
 // define("controller_default", "usuarioController");
 // define("action_default", "index");
