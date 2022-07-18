c6c1c815dab41d8dcb5a6a38d92879cacf141b69
Edwin Flores Vargas <al222110833@gmail.com>
2022-07-17 20:38:51 -0500

    Se modificaron las conexiones a la base de datos al igual que se agrego la zona horaria de mexico el cual se ocupa para ciertas funciones en las consultas

--- config/ConexionDB.php
+++ config/ConexionDB.php
@@ -1,11 +1,11 @@
 <?php 
-//  try {
-//     $PDO = new PDO('mysql:host=localhost; dbname=treninterurbano','root','');
-//     } catch (PDOException $e) {
-//         echo "Error!!".$e->getMessage();
-//    }
-   
-//    $conexion = new mysqli("localhost", "root", "", "treninterurbano");
+date_default_timezone_set("America/Mexico_City");
+  try {
+     $PDO = new PDO('mysql:host=localhost; dbname=treninterurbano','root','');
+     } catch (PDOException $e) {
+         echo "Error!!".$e->getMessage();
+    }
+    $conexion = new mysqli("localhost", "root", "", "treninterurbano");
 
 //    if ($conexion) {
 //     echo "La conexion fue exitosa a la base de datos";
@@ -15,18 +15,17 @@
 
 //    mysqli_close($conexion);
 
-try {
-    $PDO = new PDO('mysql:host=www.db4free.net; dbname=treninterurbano; port=3306','admintren','admin2022');
-    } catch (PDOException $e) {
-        echo "Error!!".$e->getMessage();
-   }
+// try {
+//     $PDO = new PDO('mysql:209.209.40.86; dbname=treninterurbano; port=19102','admin','Ad0Ox082');
+//     } catch (PDOException $e) {
+//         // echo "Error!!".$e->getMessage();
+//    }
    
-   $conexion = new mysqli("www.db4free.net", "admintren", "admin2022", "treninterurbano",3306);
+//    $conexion = new mysqli("mysql-81893-0.cloudclusters.net", "admin", "Ad0Ox082", "treninterurbano",19102);
 
-   if ($conexion) {
-    echo "La conexion fue exitosa a la base de datos";
-   }else{
-    echo "No se pudo conectar a la base de datos";
-   }
+    // if ($conexion) {
+    //  echo "La conexion fue exitosa a la base de datos";
+    // }else{
+    // echo "No se pudo conectar a la base de datos";
+    // }
 
-   mysqli_close($conexion);
