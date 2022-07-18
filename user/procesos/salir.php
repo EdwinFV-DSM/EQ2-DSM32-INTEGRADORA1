b2f3fd34ee31c393fa589b1a0a1cd7653e697c2e
Edwin Flores Vargas <al222110833@gmail.com>
2022-07-17 20:50:37 -0500

    Sistema funcionando correctamente, ya cierra correctamente la sesion

---
+++ user/procesos/salir.php
@@ -0,0 +1,11 @@
+<?php
+include_once '../../config/parameters.php';
+if (session_start()) {
+    
+}else{
+    session_start();
+}
+session_destroy();
+
+
+header('location: '.base_url.'user/login.php');
