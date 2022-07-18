47883fdcbdfb2d783e74a68fd6565fe72ed108e7
Edwin Flores Vargas <al222110833@gmail.com>
2022-07-17 20:29:49 -0500

    Modificacion del boleto en la base de datos funcionando

---
+++ admin/assets/js/modificar_boleto.js
@@ -0,0 +1,49 @@
+// console.log('Funcionando');
+//enviar formulario a la base de datos
+var formulario2 = document.getElementById('modificar-boleto');
+
+formulario2.addEventListener('submit',  function(e) {
+    e.preventDefault();
+    // console.log('Me diste un click UwU');
+
+    var datos = new FormData(formulario2);
+
+
+    // console.log(datos);
+    console.log(datos.get('cliente'))
+    console.log(datos.get('fechaC'))
+    console.log(datos.get('fechaV'))
+    console.log(datos.get('costo'))
+    console.log(datos.get('ruta'))
+    console.log(datos.get('operacion'))
+
+    fetch('../../admin/operaciones/modificar_boleto2.php',{
+        method: 'POST',
+        body: datos
+    })
+    .then( res => res.json())
+    .then( data => {
+        console.log(data)
+        if(data === 'error') {
+            Swal.fire(
+              'Error',
+              'Revise los datos ingresados',
+              'error'
+            )
+           
+        }else if(data=== 'error-insertar'){
+            Swal.fire(
+                'Error',
+                'Hubo un error al crear el boleto',
+                'error'
+              )
+        }else{
+            Swal.fire(
+                'Success',
+                'Se ha modificado correctamente el boleto',
+                'success'
+              )
+        }
+    })
+
+    });
