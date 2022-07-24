096e72d8fb8fc537d81d2c32666e55f2723cb525
Edwin Flores Vargas <al222110833@gmail.com>
2022-07-23 19:26:41 -0500

    Se modifico la alerta de inicio de sesion de error

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,8 @@
-// console.log('Funcionando');
 //enviar formulario a la base de datos
 var formulario = document.getElementById("formulario-login");
 
 formulario.addEventListener("submit", function (e) {
   e.preventDefault();
-  // console.log('Me diste un click UwU');
 
   var datos = new FormData(formulario);
 
@@ -22,11 +20,7 @@ formulario.addEventListener("submit", function (e) {
     .then((data) => {
       console.log(data);
       if (data === "error") {
-        Swal.fire(
-        "Error", 
-        "Revise los datos ingresados", 
-        "error"
-        );
+        Swal.fire("Error", "Revise los datos ingresados", "error");
       } else {
         let timerInterval;
         Swal.fire({
@@ -53,22 +47,6 @@ formulario.addEventListener("submit", function (e) {
               "http://localhost/EQ2-DSM32-INTEGRADORA1/user/panel.php";
           }
         });
-        // Swal.fire({
-        //   title: 'Te has logeado correctamente',
-        //   icon: 'success',
-        //   showCloseButton: true,
-        //   showCancelButton: true,
-        //   focusConfirm: false,
-        //   confirmButtonText:
-        //     '<a href="http://localhost/EQ2-DSM32-INTEGRADORA1/user/panel.php" style="color: white;text-decoration: none;"><i class="fa fa-thumbs-up"></i> Great!</a>',
-        //   cancelButtonAriaLabel: 'Thumbs down'
-        // })
-
-        // Swal.fire(
-        //   'Success',
-        //   'Se a registrado correctamente',
-        //   'success'
-        // )
       }
     });
 });
