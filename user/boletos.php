535017c1c509d1ba2412c20f883be0b24068149c
Edwin Flores Vargas <al222110833@gmail.com>
2022-07-23 19:33:00 -0500

    Se modifico el href del paginado, se añadio una validacion a los botones para que se desabiliten cuando se autorice el pago

--- user/boletos.php
+++ user/boletos.php
@@ -4,9 +4,8 @@ if (session_start()) {
     session_start();
 }
 
-if ($_SESSION['idTUsuario']!= 1) {
-    
-}else{
+if ($_SESSION['idTUsuario'] != 1) {
+} else {
     header('Location: ../user/panel.php');
 }
 $pagina = 0;
@@ -15,7 +14,9 @@ $video = 0;
 $nombre_pagina = "Boletos";
 require_once '../includes/header.php';
 
-$tickets = 'SELECT * FROM tickets WHERE idCliente ='.$_SESSION['idCliente'];
+
+
+$tickets = 'SELECT * FROM tickets WHERE idCliente =' . $_SESSION['idCliente'];
 $sentencia = $PDO->prepare($tickets);
 $sentencia->execute();
 
@@ -38,17 +39,17 @@ $_POST['campo'] = '';
     <div class="container">
         <div class="row shadow-lg p-3 mb-5 bg-body rounded">
             <h1 style="text-align: center;">Boletos</h1>
-            
+
             <div class="action-crear-search">
                 <!-- Button trigger modal -->
                 <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     + Crear
-                </button>                
+                </button>
 
                 <form class="d-flex" role="search" method="post">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="campo" id="campo">
-                    <button class="btn btn-outline-success" type="submit"><i class="bi bi-search"></i></button>                    
-                </form>                
+                    <button class="btn btn-outline-success" type="submit"><i class="bi bi-search"></i></button>
+                </form>
             </div>
             <table class="table">
                 <thead>
@@ -61,7 +62,7 @@ $_POST['campo'] = '';
                 </thead>
                 <!-- En este apartado empieza la parte donde se hace el paginado y se valida que el campo que es el input de busqueda no este lleno  -->
                 <?php if ($_REQUEST['campo'] == '') { ?>
-                    
+
                     <?php
                     /**Se valida que si no se esta recibiendo nada por metodo get nos rediriga al apartado que es al apartado de la paginacion */
                     if (!$_GET) {
@@ -83,12 +84,12 @@ $_POST['campo'] = '';
                      * Despues de realizar la consulta usamos la sentencia execute para poder ejecutar la consulta y poder obtener los datos de la tabla consultada
                      * y todo eso lo extraemos con la palabra reservada fetchAll, y realizamos un foreach para poder mostrar todos los datos.
                      *
-                    */
+                     */
 
                     $iniciar = ($_GET['pagina'] - 1) * $tickets_x_pagina;
                     // echo '<h1>'.$iniciar.'</h1>';
-                    
-                    $sql_tickets = 'SELECT * FROM tickets WHERE idCliente ='. $_SESSION['idCliente'] .' LIMIT :iniciar,:narticulos';
+
+                    $sql_tickets = 'SELECT * FROM tickets WHERE idCliente =' . $_SESSION['idCliente'] . ' LIMIT :iniciar,:narticulos';
 
                     $sentencia_tickets = $PDO->prepare($sql_tickets);
                     $sentencia_tickets->bindParam(':iniciar', $iniciar, PDO::PARAM_INT);
@@ -97,54 +98,62 @@ $_POST['campo'] = '';
 
                     $resultado_tickets = $sentencia_tickets->fetchAll();
                     foreach ($resultado_tickets as $tickets) : ?>
-                    
+
                         <tr>
                             <th scope="row"><?php echo $tickets['N_Operacion'] ?></th>
                             <?php
                             $querycliente = 'SELECT * FROM cliente WHERE idCliente =' . $tickets['idCliente'];
                             $cliente = mysqli_query($conexion, $querycliente);
-                            $row_cliente = mysqli_fetch_assoc($cliente); 
+                            $row_cliente = mysqli_fetch_assoc($cliente);
+
+                            $queryPagoBoleto = "SELECT * FROM pago WHERE idTicket =" . $tickets['idTicket'];
+                            $Pago = mysqli_query($conexion, $queryPagoBoleto);
+                            $row_Pago = mysqli_fetch_assoc($Pago);
+                            // var_dump($row_Pago);
                             ?>
-                            
+
                             <th scope="row"><?php echo $row_cliente['nombre'] ?> <?php echo $row_cliente['apellidos'] ?></th>
                             <th scope="row"><?php echo $tickets['fechaV'] ?></th>
                             <td>
-                                <a class="btn btn-success" href="<?= base_url ?>user/editar.php?operacion=<?php echo $tickets['N_Operacion']; ?>"><i class="bi bi-pencil-square"></i></a>
-                                <!-- <a class="btn btn-danger" href="<?= base_url ?>admin/boletos.php?pagina=<?php echo $_GET['pagina'] ?>&N_Operacion=<?php echo $tickets['N_Operacion']; ?>"><i class="bi bi-trash"></i></a> -->                                
+                                <a class="btn btn-success <?php echo $row_Pago['status'] == "COMPLETED" ? 'disabled' : '' ?>" href="<?= base_url ?>user/editar.php?operacion=<?php echo $tickets['N_Operacion']; ?>"><i class="bi bi-pencil-square"></i></a>
+                                <!-- <a class="btn btn-danger" href="<?= base_url ?>admin/boletos.php?pagina=<?php echo $_GET['pagina'] ?>&N_Operacion=<?php echo $tickets['N_Operacion']; ?>"><i class="bi bi-trash"></i></a> -->
                                 <a class="btn btn-primary" href="<?= base_url ?>admin/operaciones/consulta.php?operacion=<?php echo $tickets['N_Operacion']; ?>"><i class="bi bi-search"></i></a>
-                                <a href="<?= base_url ?>user/procesos/pagar_boleto.php?operacion=<?php echo $tickets['idTicket'] ?>" class="btn btn-warning"><i class="bi bi-currency-dollar"></i></a>
+                                <a href="<?= base_url ?>user/procesos/pagar_boleto.php?operacion=<?php echo $tickets['idTicket'] ?>" class="btn btn-warning"><i class="bi bi-currency-dollar"></i></a>                                
+                                    <a href="<?= base_url ?>temp/<?php echo $row_Pago['codigo_qr'] ?>" class="btn btn-success <?php echo $row_Pago['status'] == "COMPLETED" ? '' : 'disabled' ?>" target="_blank" download><i class="bi bi-qr-code"></i></a>
                             </td>
                         </tr>
                     <?php endforeach ?>
                     </tbody>
-                    <?php } elseif ($_REQUEST['campo'] != '') {?>
-                        <script>console.log("Funcionando");</script>
-                        <tbody class="table-group-divider" id="content">
-                         <!-- El id del cuerpo de la tabla.  -->
+                <?php } elseif ($_REQUEST['campo'] != '') { ?>
+                    <script>
+                        console.log("Funcionando");
+                    </script>
+                    <tbody class="table-group-divider" id="content">
+                        <!-- El id del cuerpo de la tabla.  -->
                     </tbody>
-                    <?php }else{ ?>
-                        
-                        <tbody class="table-group-divider" id="content">
-                         <!-- El id del cuerpo de la tabla.  -->
+                <?php } else { ?>
+
+                    <tbody class="table-group-divider" id="content">
+                        <!-- El id del cuerpo de la tabla.  -->
                     </tbody>
-                    
-        <?php } ?>
-        </table>
-        <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-center">
-                <li class="page-item <?php echo $_GET['pagina'] <= 1 ? 'disabled' : '' ?>">
-                    <a class="page-link" href="<?= base_url ?>admin/boletos.php?pagina=<?php echo $_GET['pagina'] - 1 ?>">Previous</a>
-                </li>
 
-                <?php for ($i = 0; $i < $paginas; $i++) : ?>
-                    <li class="page-item <?php echo $_GET['pagina'] == $i + 1 ? 'active' : '' ?>"><a class="page-link" href="<?= base_url ?>admin/boletos.php?pagina=<?php echo $i + 1 ?>"><?php echo $i + 1 ?></a></li>
-                <?php endfor ?>
+                <?php } ?>
+            </table>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <li class="page-item <?php echo $_GET['pagina'] <= 1 ? 'disabled' : '' ?>">
+                        <a class="page-link" href="<?= base_url ?>user/boletos.php?pagina=<?php echo $_GET['pagina'] - 1 ?>">Previous</a>
+                    </li>
 
-                <li class=" page-item <?php echo $_GET['pagina'] >= $paginas ? 'disabled' : '' ?>">
-                    <a class=" page-link" href="<?= base_url ?>admin/boletos.php?pagina=<?php echo $_GET['pagina'] + 1 ?>">Next</a>
-                </li>
-            </ul>
-        </nav>
+                    <?php for ($i = 0; $i < $paginas; $i++) : ?>
+                        <li class="page-item <?php echo $_GET['pagina'] == $i + 1 ? 'active' : '' ?>"><a class="page-link" href="<?= base_url ?>user/boletos.php?pagina=<?php echo $i + 1 ?>"><?php echo $i + 1 ?></a></li>
+                    <?php endfor ?>
+
+                    <li class=" page-item <?php echo $_GET['pagina'] >= $paginas ? 'disabled' : '' ?>">
+                        <a class=" page-link" href="<?= base_url ?>user/boletos.php?pagina=<?php echo $_GET['pagina'] + 1 ?>">Next</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </div>
 </div>
