144ed195599303d7379bf1f6bc846ab2cb8f6f70
Edwin Flores Vargas <al222110833@gmail.com>
2022-07-28 01:51:28 -0500

    No hay modificaciones como tal

--- user/procesos/signup.php
+++ user/procesos/signup.php
@@ -1,57 +1,56 @@
-<?php 
+<?php
 include_once '../../config/ConexionDB.php';
 
-if ($_POST ['nombre']=='' || $_POST['nombre'] ==' ') {
+if ($_POST['nombre'] == '' || $_POST['nombre'] == ' ') {
     echo json_encode('error-nombre');
-}elseif($_POST ['apellido']=='' || $_POST['apellido']==' '){
+} elseif ($_POST['apellido'] == '' || $_POST['apellido'] == ' ') {
     echo json_encode('error-apellido');
-}elseif($_POST ['fechaNa']=='' || $_POST['fechaNa']==' '){
+} elseif ($_POST['fechaNa'] == '' || $_POST['fechaNa'] == ' ') {
     echo json_encode('error-fechaNa');
-}elseif($_POST ['email']=='' || $_POST['email']==' '){
+} elseif ($_POST['email'] == '' || $_POST['email'] == ' ') {
     echo json_encode('error-email');
-}elseif($_POST ['password']=='' || $_POST['password']==' '){
+} elseif ($_POST['password'] == '' || $_POST['password'] == ' ') {
     echo json_encode('error-password');
-}elseif($_POST ['sexo']=='' || $_POST['sexo']==' '){
+} elseif ($_POST['sexo'] == '' || $_POST['sexo'] == ' ') {
     echo json_encode('error-sexo');
-}else{
-    $nombre=$_POST['nombre'];
-    $apellido=$_POST['apellido'];
-    $fechaNa=$_POST['fechaNa'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    $sexo=$_POST['sexo'];
+} else {
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $fechaNa = $_POST['fechaNa'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $sexo = $_POST['sexo'];
     $dateCreacion = date('Y-m-d H:i:s');
 
     /**
      * incriptacion de password
      */
-    $password_encriptada = password_hash($password,PASSWORD_DEFAULT);
+    $password_encriptada = password_hash($password, PASSWORD_DEFAULT);
 
     $querycliente = "SELECT * FROM cliente";
-    $cliente = mysqli_query($conexion,$querycliente);
+    $cliente = mysqli_query($conexion, $querycliente);
     $row_cliente = mysqli_fetch_assoc($cliente);
 
-    if($email == $row_cliente['email']){
+    if ($email == $row_cliente['email']) {
         echo json_encode('error-cuenta');
-    }else{
+    } else {
 
         if ($sexo == 'F') {
-      $insertarcliene ="INSERT INTO `cliente`(`idCliente`, `nombre`, `apellidos`, `fechaNac`, `idTUsuario`, `email`, `telefono`, `calle`, `numExt`, `numInt`, `municipio`, `codigoPostal`, `idServicio`, `idEscuela`, `Status`, `Sexo`, `password`, `dateCreacion`, `dateModificacion`, `dateEliminacion`, `img`) VALUES (NULL,'$nombre','$apellido','$fechaNa',3,'$email',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,1,'$sexo','$password_encriptada','$dateCreacion',NULL,NULL,NULL)";
-      $creacion = mysqli_query($conexion,$insertarcliene);
-      if ($creacion) {
-        echo json_encode('success');
-    } else {
-        echo json_encode('error-insertar');
-    }
-    }elseif ($sexo == 'M') {
-        $insertarcliene ="INSERT INTO `cliente`(`idCliente`, `nombre`, `apellidos`, `fechaNac`, `idTUsuario`, `email`, `telefono`, `calle`, `numExt`, `numInt`, `municipio`, `codigoPostal`, `idServicio`, `idEscuela`, `Status`, `Sexo`, `password`, `dateCreacion`, `dateModificacion`, `dateEliminacion`, `img`) VALUES (NULL,'$nombre','$apellido','$fechaNa',3,'$email',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,1,'$sexo','$password_encriptada','$dateCreacion',NULL,NULL,NULL)";
-      $creacion = mysqli_query($conexion,$insertarcliene);
-      if ($creacion) {
-        echo json_encode('success');
-    } else {
-        echo json_encode('error-insertar');
-    }
+            $insertarcliene = "INSERT INTO `cliente`(`idCliente`, `nombre`, `apellidos`, `fechaNac`, `idTUsuario`, `email`, `telefono`, `calle`, `numExt`, `numInt`, `municipio`, `codigoPostal`, `idServicio`, `idEscuela`, `Status`, `Sexo`, `password`, `dateCreacion`, `dateModificacion`, `dateEliminacion`, `img`) VALUES (NULL,'$nombre','$apellido','$fechaNa',3,'$email',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,1,'$sexo','$password_encriptada','$dateCreacion',NULL,NULL,'user2.png')";
+            $creacion = mysqli_query($conexion, $insertarcliene);
+            if ($creacion) {
+                echo json_encode('success');
+            } else {
+                echo json_encode('error-insertar');
+            }
+        } elseif ($sexo == 'M') {
+            $insertarcliene = "INSERT INTO `cliente`(`idCliente`, `nombre`, `apellidos`, `fechaNac`, `idTUsuario`, `email`, `telefono`, `calle`, `numExt`, `numInt`, `municipio`, `codigoPostal`, `idServicio`, `idEscuela`, `Status`, `Sexo`, `password`, `dateCreacion`, `dateModificacion`, `dateEliminacion`, `img`) VALUES (NULL,'$nombre','$apellido','$fechaNa',3,'$email',NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,1,'$sexo','$password_encriptada','$dateCreacion',NULL,NULL,'user.png')";
+            $creacion = mysqli_query($conexion, $insertarcliene);
+            if ($creacion) {
+                echo json_encode('success');
+            } else {
+                echo json_encode('error-insertar');
+            }
+        }
     }
 }
-}
-
