b7eef9c6fb0924d8ae5c2969a1cf3416f0492f05
Edwin Flores Vargas <al222110833@gmail.com>
2022-07-29 06:32:15 -0600

    Se agrego un nuevo fragmento de inicio de sesion donde se crea la sesion cuando se logea una escuela

--- user/procesos/login.php
+++ user/procesos/login.php
@@ -59,32 +59,22 @@ if (isset($_REQUEST['email'])) {
                     echo json_encode('success');
                     $querycliente = "SELECT * FROM escuelas WHERE email='$email'";
                     $cliente = mysqli_query($conexion, $querycliente);
-                    $row = mysqli_fetch_assoc($cliente);
+                    $row_escuelas = mysqli_fetch_assoc($cliente);
                     $_SESSION['login'] = 1;
-                    $_SESSION['idCliente'] = $row['idCliente'];
-                    $_SESSION['nombre'] = $row['nombre'];
-                    $_SESSION['apellidos'] = $row['apellidos'];
-                    $_SESSION['img'] = $row['img'];
-                    $_SESSION['idTUsuario'] = $row['idTUsuario'];
-                    $_SESSION['idEscuela'] = $row['idEscuela'];
-                    $_SESSION['status'] = $row['Status'];
-                    $_SESSION['sexo'] = $row['Sexo'];
-                    $_SESSION['dateCreacion'] = $row['dateCreacion'];
+                    $_SESSION['idEscuela'] = $row_escuelas['idEscuela'];
+                    $_SESSION['nombre'] = $row_escuelas['Nombre'];
+                    $_SESSION['img'] = $row_escuelas['img'];
+                    $_SESSION['idTUsuario'] = $row_escuelas['idTUsuario'];
                 }else if($pass == $row['password']){
                     echo json_encode('success');
-                    $querycliente = "SELECT * FROM cliente WHERE email='$email'";
+                    $querycliente = "SELECT * FROM escuelas WHERE email='$email'";
                     $cliente = mysqli_query($conexion, $querycliente);
-                    $row = mysqli_fetch_assoc($cliente);
+                    $row_escuelas = mysqli_fetch_assoc($cliente);
                     $_SESSION['login'] = 1;
-                    $_SESSION['idCliente'] = $row['idCliente'];
-                    $_SESSION['nombre'] = $row['nombre'];
-                    $_SESSION['apellidos'] = $row['apellidos'];
-                    $_SESSION['img'] = $row['img'];
-                    $_SESSION['idTUsuario'] = $row['idTUsuario'];
-                    $_SESSION['idEscuela'] = $row['idEscuela'];
-                    $_SESSION['status'] = $row['Status'];
-                    $_SESSION['sexo'] = $row['Sexo'];
-                    $_SESSION['dateCreacion'] = $row['dateCreacion'];
+                    $_SESSION['idEscuela'] = $row_escuelas['idEscuela'];
+                    $_SESSION['nombre'] = $row_escuelas['Nombre'];
+                    $_SESSION['img'] = $row_escuelas['img'];
+                    $_SESSION['idTUsuario'] = $row_escuelas['idTUsuario'];
                 } else {
                     echo json_encode('error');
                 }
